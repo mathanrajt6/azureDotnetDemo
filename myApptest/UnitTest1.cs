f9de53577099ff910f0a44d2c12aabfec7360f8b
Mathanraj <mathanraj.kanini@gmail.com>
2023-10-17 10:55:45 +0530

    Initial commit

---
+++ myApptest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace myApptest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
